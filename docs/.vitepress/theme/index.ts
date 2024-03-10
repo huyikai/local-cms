95e0ab0b101bc810fce14ff6a84839604e4c5f9e
huyikai <hyk_tiger@foxmail.com>
2024-03-10 16:30:44 +0800

    refactor: 💡 vitepress config

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,21 +1,9 @@
-import 'vitepress/dist/client/theme-default/styles/vars.css';
-import 'vitepress/dist/client/theme-default/styles/base.css';
-import 'vitepress/dist/client/theme-default/styles/utils.css';
-import 'vitepress/dist/client/theme-default/styles/components/custom-block.css';
-import 'vitepress/dist/client/theme-default/styles/components/vp-code.css';
-import 'vitepress/dist/client/theme-default/styles/components/vp-code-group.css';
-import 'vitepress/dist/client/theme-default/styles/components/vp-doc.css';
-import 'vitepress/dist/client/theme-default/styles/components/vp-sponsor.css';
-
 import Home from './home.vue';
-import Layout from '@huyikai/vitepress-helper/theme/Theme.vue';
-import VPBadge from 'vitepress/dist/client/theme-default/components/VPBadge.vue';
+import theme from '@huyikai/vitepress-helper/theme/index';
 
-const theme = {
-  Layout,
+export default {
+  extends: theme,
   enhanceApp: ({ app }: any) => {
     app.component('Home', Home);
-    app.component('Badge', VPBadge);
   }
 };
-export default theme;
